3562095bac5a1d6cab3f155f40316c1c5db81580
Leo-Wagner <m256606@usna.edu>
2022-11-08 09:25:45 -0500

    Alien Invasion through Chapter 13

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings:
         self.bg_color = (174, 15, 255)
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (214, 99, 36)
@@ -17,3 +17,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
